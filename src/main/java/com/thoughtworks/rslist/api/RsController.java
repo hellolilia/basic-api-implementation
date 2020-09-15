d30aab3b9d4aa2d19abd7a9374a69dc35f604264
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-09-15 22:33:37 +0800

    should get one rs event

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,11 +1,29 @@
 package com.thoughtworks.rslist.api;
 
+import domain.RsEvent;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class RsController {
-  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+  private List<RsEvent> rsList = initRsEvent();
+
+  private List<RsEvent> initRsEvent() {
+    List<RsEvent> rsEventList = new ArrayList<>();
+    rsEventList.add(new RsEvent("第一条事件", "无标签"));
+    rsEventList.add(new RsEvent("第二条事件", "无标签"));
+    rsEventList.add(new RsEvent("第三条事件", "无标签"));
+    return rsEventList;
+  }
+
+  @GetMapping("/rs/{index}")
+  public RsEvent getOneRsEvent(@PathVariable int index){
+    return rsList.get(index - 1);
+  }
+
 }
---
+++ src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
@@ -0,0 +1,38 @@
+package com.thoughtworks.rslist.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class RsControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void should_get_one_rs_event() throws Exception {
+        mockMvc.perform(get("/rs/1"))
+                .andExpect(jsonPath("$.eventName", is("第一条事件")))
+                .andExpect(jsonPath("$.keyWord", is("无标签")))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/rs/2"))
+                .andExpect(jsonPath("$.eventName", is("第二条事件")))
+                .andExpect(jsonPath("$.keyWord", is("无标签")))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/rs/3"))
+                .andExpect(jsonPath("$.eventName", is("第三条事件")))
+                .andExpect(jsonPath("$.keyWord", is("无标签")))
+                .andExpect(status().isOk());
+    }
+
+
+}
